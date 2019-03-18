167985759b1d036e58e70ea16950284fa24ad7e2
yingshiwu1997 <yingshi.wu@galileo.edu>
2019-03-17 23:42:24 -0600

    cambios

--- src/app/historial/historial.module.ts
+++ src/app/historial/historial.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule,Router } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
 
 import { HistorialPage } from './historial.page';
