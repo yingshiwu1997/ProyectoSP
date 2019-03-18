89234cb8fec6f76c78135d0f79de96e5ba4a8b69
jjjramirez <jaime.ramirez@galileo.edu>
2019-03-17 23:41:42 -0600

    Firebase Auth
    Login.

--- src/app/iniciar/iniciar.page.ts
+++ src/app/iniciar/iniciar.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/user';
+import { AngularFireAuth } from 'angularfire2/auth'
+import { NavController } from '@ionic/angular';
+ 
 @Component({
   selector: 'app-iniciar',
   templateUrl: './iniciar.page.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciarPage implements OnInit {
 
-  constructor() { }
+  user = {} as User;
+
+  constructor(private afAuth: AngularFireAuth, public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  async login(){
+    this.afAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+    .then(
+      result => {
+        console.log(result);
+        this.navCtrl.navigateForward('/tabs');
+      } 
+    )
+    .catch(err => console.log(err));
   }
 
 }
