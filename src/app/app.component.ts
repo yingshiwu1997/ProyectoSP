b112cfbba08e5b0df0df5d3d69e830ac38f9b608
yingshiwu1997 <yingshi.wu@galileo.edu>
2019-05-12 14:41:08 -0600

    Parte grafica comprar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +14,20 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private router : Router
+    private router : Router,
+    private afauth: AngularFireAuth
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.router.navigateByUrl('iniciar');
+      console.log(this.afauth.auth.currentUser);
+      if(this.afauth.auth.currentUser == null){
+        this.router.navigateByUrl('iniciar');
+      }else{
+        this.router.navigateByUrl('tabs');
+      }
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
